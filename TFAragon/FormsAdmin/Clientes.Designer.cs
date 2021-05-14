be0221fe23ac06ff4cfb5ef45e15ab1c8f1e02ed
BenjaminVelasco95 <84163988+BenjaminVelasco95@users.noreply.github.com>
2021-05-13 22:01:52 -0500

    TFAragonV1
    Primer entrega del sistema

---
+++ TFAragon/FormsAdmin/Clientes.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace TFAragon
+{
+    partial class Clientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGestionar = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dtgClientes = new System.Windows.Forms.DataGridView();
+            this.lblActualizar = new System.Windows.Forms.Label();
+            this.minimizar = new System.Windows.Forms.PictureBox();
+            this.cerrar = new System.Windows.Forms.PictureBox();
+            this.barDesplazamiento = new System.Windows.Forms.PictureBox();
+            this.btnDetalle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIC = new System.Windows.Forms.TextBox();
+            this.btnExpediente = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgClientes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minimizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cerrar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barDesplazamiento)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label3.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 15);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "Clientes";
+            // 
+            // btnGestionar
+            // 
+            this.btnGestionar.BackColor = System.Drawing.Color.DarkCyan;
+            this.btnGestionar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGestionar.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Bold);
+            this.btnGestionar.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnGestionar.Location = new System.Drawing.Point(51, 79);
+            this.btnGestionar.Name = "btnGestionar";
+            this.btnGestionar.Size = new System.Drawing.Size(75, 50);
+            this.btnGestionar.TabIndex = 34;
+            this.btnGestionar.Text = "Gestionar Clientes";
+            this.btnGestionar.UseVisualStyleBackColor = false;
+            this.btnGestionar.Click += new System.EventHandler(this.btnGestionar_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnGestionar);
+            this.groupBox2.Controls.Add(this.btnDetalle);
+            this.groupBox2.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Bold);
+            this.groupBox2.Location = new System.Drawing.Point(12, 88);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(331, 197);
+            this.groupBox2.TabIndex = 35;
+            this.groupBox2.TabStop = false;
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.BackColor = System.Drawing.Color.DarkCyan;
+            this.btnRegresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegresar.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Bold);
+            this.btnRegresar.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnRegresar.Location = new System.Drawing.Point(12, 57);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(75, 25);
+            this.btnRegresar.TabIndex = 35;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.UseVisualStyleBackColor = false;
+            this.btnRegresar.Click += new System.EventHandler(this.btnRegresar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dtgClientes);
+            this.groupBox1.Controls.Add(this.lblActualizar);
+            this.groupBox1.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Bold);
+            this.groupBox1.Location = new System.Drawing.Point(12, 291);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(676, 197);
+            this.groupBox1.TabIndex = 36;
+            this.groupBox1.TabStop = false;
+            // 
+            // dtgClientes
+            // 
+            this.dtgClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgClientes.Location = new System.Drawing.Point(6, 17);
+            this.dtgClientes.Name = "dtgClientes";
+            this.dtgClientes.Size = new System.Drawing.Size(664, 174);
+            this.dtgClientes.TabIndex = 0;
+            this.dtgClientes.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgClientes_CellContentClick);
+            // 
+            // lblActualizar
+            // 
+            this.lblActualizar.AutoSize = true;
+            this.lblActualizar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblActualizar.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblActualizar.Location = new System.Drawing.Point(6, -1);
+            this.lblActualizar.Name = "lblActualizar";
+            this.lblActualizar.Size = new System.Drawing.Size(55, 15);
+            this.lblActualizar.TabIndex = 29;
+            this.lblActualizar.Text = "Clientes";
+            this.lblActualizar.Click += new System.EventHandler(this.lblActualizar_Click);
+            // 
+            // minimizar
+            // 
+            this.minimizar.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.minimizar.Image = global::TFAragon.Properties.Resources.minimizar;
+            this.minimizar.Location = new System.Drawing.Point(633, 0);
+            this.minimizar.Name = "minimizar";
+            this.minimizar.Size = new System.Drawing.Size(30, 30);
+            this.minimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.minimizar.TabIndex = 28;
+            this.minimizar.TabStop = false;
+            this.minimizar.Click += new System.EventHandler(this.minimizar_Click);
+            // 
+            // cerrar
+            // 
+            this.cerrar.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.cerrar.Image = global::TFAragon.Properties.Resources.cerrar;
+            this.cerrar.Location = new System.Drawing.Point(669, 0);
+            this.cerrar.Name = "cerrar";
+            this.cerrar.Size = new System.Drawing.Size(30, 30);
+            this.cerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cerrar.TabIndex = 27;
+            this.cerrar.TabStop = false;
+            this.cerrar.Click += new System.EventHandler(this.cerrar_Click);
+            // 
+            // barDesplazamiento
+            // 
+            this.barDesplazamiento.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.barDesplazamiento.Dock = System.Windows.Forms.DockStyle.Top;
+            this.barDesplazamiento.Location = new System.Drawing.Point(0, 0);
+            this.barDesplazamiento.Name = "barDesplazamiento";
+            this.barDesplazamiento.Size = new System.Drawing.Size(700, 30);
+            this.barDesplazamiento.TabIndex = 26;
+            this.barDesplazamiento.TabStop = false;
+            this.barDesplazamiento.MouseDown += new System.Windows.Forms.MouseEventHandler(this.barDesplazamiento_MouseDown);
+            this.barDesplazamiento.MouseMove += new System.Windows.Forms.MouseEventHandler(this.barDesplazamiento_MouseMove);
+            this.barDesplazamiento.MouseUp += new System.Windows.Forms.MouseEventHandler(this.barDesplazamiento_MouseUp);
+            // 
+            // btnDetalle
+            // 
+            this.btnDetalle.BackColor = System.Drawing.Color.DarkCyan;
+            this.btnDetalle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDetalle.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Bold);
+            this.btnDetalle.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnDetalle.Location = new System.Drawing.Point(195, 79);
+            this.btnDetalle.Name = "btnDetalle";
+            this.btnDetalle.Size = new System.Drawing.Size(75, 50);
+            this.btnDetalle.TabIndex = 39;
+            this.btnDetalle.Text = "Detalle";
+            this.btnDetalle.UseVisualStyleBackColor = false;
+            this.btnDetalle.Click += new System.EventHandler(this.btnDetalle_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(98, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 15);
+            this.label1.TabIndex = 37;
+            this.label1.Text = "IC";
+            // 
+            // txtIC
+            // 
+            this.txtIC.Location = new System.Drawing.Point(101, 132);
+            this.txtIC.Name = "txtIC";
+            this.txtIC.Size = new System.Drawing.Size(123, 23);
+            this.txtIC.TabIndex = 36;
+            // 
+            // btnExpediente
+            // 
+            this.btnExpediente.BackColor = System.Drawing.Color.DarkCyan;
+            this.btnExpediente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExpediente.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Bold);
+            this.btnExpediente.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnExpediente.Location = new System.Drawing.Point(117, 61);
+            this.btnExpediente.Name = "btnExpediente";
+            this.btnExpediente.Size = new System.Drawing.Size(80, 25);
+            this.btnExpediente.TabIndex = 38;
+            this.btnExpediente.Text = "Expediente";
+            this.btnExpediente.UseVisualStyleBackColor = false;
+            this.btnExpediente.Click += new System.EventHandler(this.btnExpediente_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.btnExpediente);
+            this.groupBox3.Controls.Add(this.txtIC);
+            this.groupBox3.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Bold);
+            this.groupBox3.Location = new System.Drawing.Point(357, 88);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(331, 197);
+            this.groupBox3.TabIndex = 36;
+            this.groupBox3.TabStop = false;
+            // 
+            // Clientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(700, 500);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnRegresar);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.minimizar);
+            this.Controls.Add(this.cerrar);
+            this.Controls.Add(this.barDesplazamiento);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Clientes";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GestionClientes";
+            this.Load += new System.EventHandler(this.Clientes_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgClientes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minimizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cerrar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barDesplazamiento)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox minimizar;
+        private System.Windows.Forms.PictureBox cerrar;
+        private System.Windows.Forms.PictureBox barDesplazamiento;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGestionar;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnRegresar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblActualizar;
+        public System.Windows.Forms.DataGridView dtgClientes;
+        private System.Windows.Forms.Button btnDetalle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIC;
+        private System.Windows.Forms.Button btnExpediente;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
---
+++ TFAragon/FormsAdmin/GestionarClientes.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace TFAragon
+{
+    public partial class GestionarClientes : Form
+    {
+        public GestionarClientes()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /*Botonose de la barra superior*/
+        private void cerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void minimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        /*movimiento de la ventana*/
+        int m = 0, mx, my;
+
+        private void barDesplazamiento_MouseDown(object sender, MouseEventArgs e)
+        {
+            m = 1;
+            mx = e.X;
+            my = e.Y;
+        }
+
+        private void barDesplazamiento_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (m == 1)
+                this.SetDesktopLocation(MousePosition.X - mx, MousePosition.Y - my);
+        }
+
+        private void barDesplazamiento_MouseUp(object sender, MouseEventArgs e)
+        {
+            m = 0;
+        }
+
+        static string conn = "SERVER = b6uzer3uyljskeemi0nr-mysql.services.clever-cloud.com; PORT=3306;DATABASE=b6uzer3uyljskeemi0nr;UID=uxbxj6okoaaumlr0;PWD=tZ5XuHvRCMFXxppdCXIU;";
+        MySqlConnection cn = new MySqlConnection(conn);
+        MySqlCommand cmd = new MySqlCommand();
+        MySqlConnection conectanos = new MySqlConnection();
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Clientes dt = new Clientes();
+            try
+            {
+                cn.Open();
+                cmd.Connection = cn;
+                cmd.CommandText = ("INSERT INTO `cliente`(`IC`, `nombre`, `apellidos`, `telefono`, `direccion`, `usuarioacceso`, `passwordacceso`) VALUES ('"+txtIC.Text+"','"+txtNombre.Text+"','"+txtApellido.Text+"','"+txtTelefono.Text+"','"+txtDireccion.Text+"','"+txtNUsuario.Text+"','"+txtClaveacceso.Text+"');");
+                MySqlDataReader dr = cmd.ExecuteReader();
+                MessageBox.Show("Se ah agregado con Exito");
+                cn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ah Ocurrido un Error: " + ex.ToString());
+            }
+            dt.dtgClientes.DataSource = dt.llenar_Grid();
+            this.Close();
+
+        }
+
+        private void btnMod_Click(object sender, EventArgs e)
+        {
+            Clientes dt = new Clientes();
+            try
+            {
+                cn.Open();
+                cmd.Connection = cn;
+                cmd.CommandText = ("UPDATE cliente SET nombre='" + txtNombre.Text + "', apellidos='" + txtApellido.Text + "', telefono='" + txtTelefono.Text + "', direccion='" + txtDireccion.Text + "' WHERE IC ='" + txtIC.Text + "',`usuarioacceso`='"+txtNUsuario.Text+"',`passwordacceso`='"+txtClaveacceso.Text+"';");
+                MySqlDataReader dr = cmd.ExecuteReader();
+                MessageBox.Show("Se ah Modificado con Exito");
+                cn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ah Ocurrido un Error: " + ex.ToString());
+            }
+            dt.dtgClientes.DataSource = dt.llenar_Grid();
+            this.Close();
+        }
+
+        private void btnDell_Click(object sender, EventArgs e)
+        {
+            Clientes dt = new Clientes();
+            try
+            {
+                cn.Open();
+                cmd.Connection = cn;
+                cmd.CommandText = ("DELETE FROM cliente WHERE IC = '" + txtIC.Text + "'");
+                MySqlDataReader dr = cmd.ExecuteReader();
+                MessageBox.Show("Se ah Eliminado con Exito");
+                cn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ah Ocurrido un Error: " + ex.ToString());
+            }
+            dt.dtgClientes.DataSource = dt.llenar_Grid();
+            this.Close();
+        }
+
+        private void GestionarClientes_Load(object sender, EventArgs e)
+        {
+            txtIC.Text = "TFC";
+        }
+    }
+}
